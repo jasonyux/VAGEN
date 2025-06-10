f8161829b108b644023b4b92cece2cb2372ada6d
root <root@dgx-16.cm.cluster>
2025-06-10 17:21:21 +0000

    updated sokoban inference script

--- scripts/inference/vllm_sokoban/run_single.sh
+++ scripts/inference/vllm_sokoban/run_single.sh
@@ -11,13 +11,20 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Extract experiment name from the path
 # This will take the last 3 parts of the path: format/sokoban/free_think
 EXPERIMENT_NAME=$(echo $SCRIPT_DIR | rev | cut -d'/' -f1-2 | rev | tr '/' '-')
-test_path=data/sokoban-terminal-vision/test.parquet
-
 echo "Experiment name: $EXPERIMENT_NAME"
 # run python -m vagen.server.server in a tmux session first
 
-python -m vagen.inference.run_inference \
+test_path=data/sokoban-terminal-vision/test.parquet
+MODEL_PATH=checkpoints/dyna_rl/ppo_sokoban_terminal_longtraj-use_loss_maskTrue-use_gae_maskTrue/global_step_200/checkpoint-200-actor
+MODEL_ID=qwen2.5-vl-3b-ppo-longtraj
+
+python -m vagen.inference.run_single_inference \
     --wandb_path_name=sokoban-terminal-vision \
     --inference_config_path="$SCRIPT_DIR/inference_config.yaml" \
-    --model_config_path="$SCRIPT_DIR/model_config.yaml" \
-    --val_files_path=$test_path
+    --val_files_path=$test_path \
+    --model_id=$MODEL_ID \
+    --model_name=$MODEL_PATH \
+    --max_tokens=2048 \
+    --top_p=0.95 \
+    --temperature=0.7 \
+    --tensor_parallel_size=2
